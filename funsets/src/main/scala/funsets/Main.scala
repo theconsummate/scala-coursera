ceae8910e305795b19aece1745c051dc915f294a
Dhruv Mishra <dhruvmishra@outlook.com>
2016-07-04 18:12:46 +0530

    init commit. finished assignment 1 and 2

---
+++ funsets/src/main/scala/funsets/Main.scala
@@ -0,0 +1,8 @@
+package funsets
+
+object Main extends App {
+  import FunSets._
+  println(contains(singletonSet(1), 1))
+  println(contains(singletonSet(1), 2))
+  println(forall(singletonSet(1), singletonSet(1)))
+}
